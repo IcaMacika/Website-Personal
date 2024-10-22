60ac78b667c2d56db1a90e47b7f3bf4ca349ab78
IcaMacika <icamacika@gmail.com>
2024-10-22 08:19:36 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// toggle
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar'); 
+ 
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
